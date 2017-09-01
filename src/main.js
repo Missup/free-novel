1d97aebf32601fe0867a6f70475aefc73f25e489
tianchen <tianchen@landhightech.com>
2017-09-01 18:04:12 +0800

    [dev]-首页推荐页面

--- src/main.js
+++ src/main.js
@@ -10,7 +10,14 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 /* eslint-disable no-new */
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>
-  	<div>
+  <div class="home">
+  	<div class="nav flex">
   		<!-- 使用 router-link 组件来导航. -->
       <!-- 通过传入 `to` 属性指定链接. -->
       <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-      <router-link to="/home/recommend">推荐</router-link>
-      <router-link to="/home/rank">排行</router-link>
+      <router-link to="/home/recommend" class="item-flex">推荐</router-link>
+      <router-link to="/home/rank" class="item-flex">排行</router-link>
   	</div>
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
   </div>
 </template>
+<style scoped>
+.nav {
+  background: #9BCD9B;
+  height: 1.8rem;
+  line-height: 1.8rem;
+}
+.nav a {
+  text-align: center;
+  color: #F0FFFF;
+}
+</style>
