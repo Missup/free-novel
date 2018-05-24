0de508f1b531e32486fa6dbb3afb4e361e43bba7
chent <chentian@linkstec.com>
2018-05-24 17:45:57 +0800

    [misc]-测试

---
+++ src/page/login/test.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <ul class="content">
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+      <li>哈哈哈哈</li>
+    </ul>
+  </div>
+</template>
+<script>
+import BScroll from 'better-scroll'
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+  },
+  created () {
+  },
+  mounted () {
+    setTimeout(() => {
+      this.initScroll()
+    }, 20)
+  },
+  methods: {
+    initScroll () {
+      let options = {
+        scrollY: true,
+        click: true,
+        scrollbar: {
+          fade: true,
+          interactive: false // 1.8.0 新增
+        },
+        mouseWheel: {
+          speed: 20,
+          invert: false,
+          easeTime: 300
+        }
+      }
+      this.scroll = new BScroll(this.$refs.wrapper, options)
+    }
+  }
+}
+</script>
+<style scoped>
+.wrapper {
+  width: 400px;
+  height: 500px;
+  margin: 0 auto;
+  background: pink;
+  overflow: hidden;
+}
+</style>
